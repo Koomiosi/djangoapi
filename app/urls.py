bb10f67e238e6951fb6baa763c38dd52b88ee185
Koomis <tonykelanen@gmail.com>
2025-10-27 08:29:51 +0200

    Views and urls configurations

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.urls import include,path
 from rest_framework import routers
+from . import views
+
 router = routers.DefaultRouter()
+router.register(r"opettajat", views.OpettajaViewSet, "nimi")
+router.register(r"kurssit", views.KurssiViewSet, "id")
 
 urlpatterns = [
-    path("api/", include((router.urls, "app"))),
+    path("", include((router.urls, "app"))),
 ]
