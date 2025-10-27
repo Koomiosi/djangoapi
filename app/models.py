c36281187badeaaea43352c9d932a645a168aacf
Koomis <tonykelanen@gmail.com>
2025-10-27 08:08:19 +0200

    Models and migrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Opettaja(models.Model):
+    nimi = models.CharField(max_length=100, default="")
+    puhelin = models.CharField(max_length=20, default="")
+
+    class Meta:
+        ordering = ['nimi']
+
+class Kurssi(models.Model):
+    nimi = models.CharField(max_length=100, default='')
+    laajuus = models.IntegerField()
+    opettaja = models.ForeignKey(Opettaja, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['nimi']
